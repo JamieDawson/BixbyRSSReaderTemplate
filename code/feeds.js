a40ad6a24e44d97a44a77e92130c8c5aed9bd10f
Maljean <maljean@student.42.us.org>
2019-10-14 22:55:58 -0700

    Added on promp training for category selection

--- code/feeds.js
+++ code/feeds.js
@@ -3,9 +3,9 @@
 // urlText: Text  for hyperlink shown at bottom of article (Required)
 // Text: What users see in feed selection list (Required)
 // Description: A description of what this is link contains.(Optional)
-// Tags: What the user can say to automatically call certain feeds (Required)
 // image: image shown next to items in category list (can be a local path or directory) (Optional)
 // Copyright: Feeds copyright message to use if the feed doesn't have one (Optional)
+// Tags: What the user can say to automatically call certain feeds (Required)
 module.exports = module.exports = [
   {
     url: 'https://thefantasyfootballers.libsyn.com/fantasyfootball',
